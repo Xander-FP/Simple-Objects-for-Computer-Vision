3c327e947d6de7497776012302086462aa5b1837
Xander Coetzer <xdilo.coetzer@gmail.com>
2023-10-28 10:47:35 +0200

    Push results

--- Main.py
+++ Main.py
@@ -26,26 +26,68 @@ RESNET_CIFAR_EXPERIMENT_1 = 'ResNet/Cifar/Experiment1'
 RESNET_CIFAR_EXPERIMENT_2 = 'ResNet/Cifar/Experiment2'
 RESNET_CIFAR_PRETRAINED = 'ResNet/Cifar/Pretrained'
 
+def plot_train_final(base1, base2, experiment1, experiment2, pretrained):
+    fig, ax = plt.subplots()
+    # Customize the plot (add labels, title, etc.)
+    ax.set_xlabel('X-axis Label')
+    ax.set_ylabel('Y-axis Label')
+    ax.set_title('Combined Graphs with Legend')
+    Graphing.plot_loss(base1, ax, 'Base1')
+    Graphing.plot_loss(base2, ax, 'Base2')
+    Graphing.plot_loss(experiment1, ax, 'Experiment1')
+    Graphing.plot_loss(experiment2, ax, 'Experiment2')
+    Graphing.plot_loss(pretrained, ax, 'Pretrained')
+
+    ax.legend()
+    plt.show()
+
+def plot_train_generated(experiment1, experiment2):
+    _, ax1 = plt.subplots()
+    _, ax2 = plt.subplots()
+    ax1.set_title('Generated set 1')
+    ax2.set_title('Generated set 2')
+    Graphing.plot_cl(experiment1, ax1, ax2, 'Experiment1', 'Valid_Loss')
+    Graphing.plot_cl(experiment2, ax1, ax2, 'Experiment2', 'Valid_Loss')
+    ax1.legend()
+    ax2.legend()
+    plt.show()
+
 reader = JSONFileReader(os.path.join(RESULTS_PATH,ALEX_BRAIN_EXPERIMENT_1))
 
-reader.fix_all_files()
+base1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_BASE_1))
+base2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_BASE_2))
+experiment1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_EXPERIMENT_1))
+experiment2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_EXPERIMENT_2))
+pretrained = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_PRETRAINED))
 
-# base1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_BASE_1))
-# base2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_BASE_2))
-# experiment1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_EXPERIMENT_1))
-# experiment2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_EXPERIMENT_2))
-# pretrained = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_BRAIN_PRETRAINED))
+# base1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_BRAIN_BASE_1))
+# base2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_BRAIN_BASE_2))
+# experiment1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_BRAIN_EXPERIMENT_1))
+# experiment2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_BRAIN_EXPERIMENT_2))
+# pretrained = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_BRAIN_PRETRAINED))
 
-# fig, ax = plt.subplots()
+# base1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_CIFAR_BASE_1))
+# base2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_CIFAR_BASE_2))
+# experiment1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_CIFAR_EXPERIMENT_1))
+# experiment2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_CIFAR_EXPERIMENT_2))
+# pretrained = reader.read_all_json_files(os.path.join(RESULTS_PATH,ALEX_CIFAR_PRETRAINED))
 
-# Add a legend
-# ax.legend()
+# base1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_CIFAR_BASE_1))
+# base2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_CIFAR_BASE_2))
+# experiment1 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_CIFAR_EXPERIMENT_1))
+# experiment2 = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_CIFAR_EXPERIMENT_2))
+# pretrained = reader.read_all_json_files(os.path.join(RESULTS_PATH,RESNET_CIFAR_PRETRAINED))
 
-# Customize the plot (add labels, title, etc.)
-# ax.set_xlabel('X-axis Label')
-# ax.set_ylabel('Y-axis Label')
-# ax.set_title('Combined Graphs with Legend')
-# Graphing.plot_accuracy(data)
+plot_train_final(base1, base2, experiment1, experiment2, pretrained)
+# plot_train_generated(experiment1, experiment2)
+
+# Graphing.show_test_data(base1, 'Base1')
+# Graphing.show_test_data(base2, 'Base2')
+# Graphing.show_test_data(experiment1, 'Experiment1')
+# Graphing.show_test_data(experiment2, 'Experiment2')
+# Graphing.show_test_data(pretrained, 'Pretrained')
+
+
 # Graphing.plot_loss(data)
 
 # df = Graphing.json_to_df(data)
--- graphing.py
+++ graphing.py
@@ -10,23 +10,78 @@ class Graphing:
         df = df.rename(columns={'index': 'file_name'})
         return df
     
-    def plot_accuracy(json_data, axis):
+    def plot_accuracy(json_data, axis, label):
         all_training_dfs = Graphing._extract_training_as_df(json_data)
         mean_df = all_training_dfs.groupby('Epoch').mean()
         std_df = all_training_dfs.groupby('Epoch').std()
 
-        axis.plot(mean_df.index, mean_df['Accuracy'])
-        axis.fill_between(mean_df.index, mean_df['Accuracy'] - std_df['Accuracy'], mean_df['Accuracy'] + std_df['Accuracy'], color='blue', alpha=0.2)
+        axis.plot(mean_df.index, mean_df['Accuracy'], label=label)
+        axis.fill_between(mean_df.index, mean_df['Accuracy'] - std_df['Accuracy'], mean_df['Accuracy'] + std_df['Accuracy'], alpha=0.2)
 
-    def plot_loss(json_data):
+    def plot_loss(json_data, axis, label):
         all_training_dfs = Graphing._extract_training_as_df(json_data)
         mean_df = all_training_dfs.groupby('Epoch').mean()
         std_df = all_training_dfs.groupby('Epoch').std()
 
-        plt.plot(mean_df.index, mean_df['Valid_Loss'])
-        plt.fill_between(mean_df.index, mean_df['Valid_Loss'] - std_df['Valid_Loss'], mean_df['Valid_Loss'] + std_df['Valid_Loss'], color='blue', alpha=0.2)
-        plt.show()
+        axis.plot(mean_df.index, mean_df['Valid_Loss'], label=label)
+        axis.fill_between(mean_df.index, mean_df['Valid_Loss'] - std_df['Valid_Loss'], mean_df['Valid_Loss'] + std_df['Valid_Loss'], alpha=0.2)
 
+    def plot_cl(json_data, axis1, axis2, label, attribute):
+        generated1_df, generated2_df = Graphing._extract_cl_as_df(json_data)
+        mean_df1 = generated1_df.groupby('Epoch').mean()
+        std_df1 = generated1_df.groupby('Epoch').std()
+        mean_df2 = generated2_df.groupby('Epoch').mean()
+        std_df2 = generated2_df.groupby('Epoch').std()
+
+        axis1.plot(mean_df1.index, mean_df1[attribute], label=label)
+        axis2.plot(mean_df2.index, mean_df2[attribute], label=label)
+
+        axis1.fill_between(mean_df1.index, mean_df1[attribute] - std_df1[attribute], mean_df1[attribute] + std_df1[attribute], alpha=0.2)
+        axis2.fill_between(mean_df2.index, mean_df2[attribute] - std_df2[attribute], mean_df2[attribute] + std_df2[attribute], alpha=0.2)
+
+    def show_test_data(json_data, label):
+        test_dfs = Graphing._extract_test_as_df(json_data)
+        print(test_dfs)
+        mean = test_dfs.mean()
+        std = test_dfs.std()
+        max = test_dfs.max()
+        median = test_dfs.median()
+
+        with open('test_data.txt', 'a') as file:
+            file.write('*******************' + label + '*******************\n')
+            file.write('Mean:\n')
+            file.write(str(mean))
+            file.write('\nMedian:\n')
+            file.write(str(median))
+            file.write('\nStd:\n')
+            file.write(str(std))
+            file.write('\nMax:\n')
+            file.write(str(max))
+            file.write('\n\n')
+
+    def _extract_test_as_df(json_data):
+        test_dfs = pd.DataFrame()
+        for file in json_data:
+            data = json_data[file]
+            filtered_data = [obj['test'] for obj in data if "test" in obj]
+
+            df = pd.DataFrame(filtered_data)
+            test_dfs = pd.concat([test_dfs, df])
+        return test_dfs
+
+    def _extract_cl_as_df(json_data):
+        generated_dfs1 = pd.DataFrame()
+        generated_dfs2 = pd.DataFrame()
+        for file in json_data:
+            data1, data2 = Graphing._get_initial_train(json_data[file])
+            filtered_data1 = [obj for obj in data1 if "Epoch" in obj and isinstance(obj["Epoch"], int)]
+            filtered_data2 = [obj for obj in data2 if "Epoch" in obj and isinstance(obj["Epoch"], int)]
+
+            df1 = pd.DataFrame(filtered_data1)
+            df2 = pd.DataFrame(filtered_data2)
+            generated_dfs1 = pd.concat([generated_dfs1, df1])
+            generated_dfs2 = pd.concat([generated_dfs2, df2])
+        return generated_dfs1, generated_dfs2
 
     def _extract_training_as_df(json_data):
         all_training_dfs = pd.DataFrame()
@@ -40,6 +95,19 @@ class Graphing:
             df = pd.DataFrame(filtered_data)
             all_training_dfs = pd.concat([all_training_dfs, df])
         return all_training_dfs
+    
+    def _get_initial_train(json_data):
+        data1 = []
+        data2 = []
+        sets = 0
+        for obj in json_data:
+            if sets == 0 or "test" in obj:
+                data1.append(obj)
+            if sets == 1 or "test" in obj:
+                data2.append(obj)
+            if "Epoch" in obj and obj["Epoch"] == "None":
+                sets += 1
+        return data1, data2
     
     def _get_final_train(json_data):
         data = []
