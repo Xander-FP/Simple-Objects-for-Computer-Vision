22a2f7929fa642ed044b3174c34948fb3b969d93
Xander Coetzer <xdilo.coetzer@gmail.com>
2023-11-03 21:50:20 +0200

    Get classifications working again

--- Code/Main_test.py
+++ Code/Main_test.py
@@ -30,14 +30,13 @@ if seed is not None:
     torch.backends.cudnn.benchmark = False
     
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(device)
 
 OPTIONS = {
         'dataset_name': 'Crop',
-        'regression': True,
+        'regression': False,
         'architecture': 'AlexNet', # 'ViT' or 'AlexNet'
         'epochs': 1, # Cifar10: 80, Brain: 70
-        'batch_size': 256,
+        'batch_size': 128,
         'learning_rate': 0.0001, # Res_Cifar: 0.006, Alex_Cifar: 0.0002, Alex_brain: 0.001, Res_brain: 0.0003
         'weight_decay': 0.002, # 0.002 Res_Cifar: 0.002, Alex_Cifar: 0.008, Alex_brain: 0.03, Res_brain: 0.01
         'momentum': 0.15, # Res_Cifar: 0.15, Alex_Cifar: 0.9, Alex_brain: 0.69, Res_brain: 0.9
@@ -86,7 +85,7 @@ def experiment(conf):
     loss, model, res_file = trainer.start(conf, tune, wandb)
 
     if not conf['should_tune']:
-        trainer.test(model, conf['batch_size'], conf['criterion'])
+        trainer.test(model, conf['batch_size'], conf['criterion'], conf['regression'])
 
     return loss
 
