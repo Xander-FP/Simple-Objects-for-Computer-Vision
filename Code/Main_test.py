Fix the normalisation

Code/Main_test.py
@@ -32,7 +32,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 OPTIONS = {
         'dataset_name': 'Crop', # 'Cifar10' or 'Brain'
-        'architecture': 'ViT', # 'ResNet' or 'AlexNet'
+        'architecture': 'AlexNet', # 'ResNet' or 'AlexNet'
         'epochs': 80, # Cifar10: 80, Brain: 70
         'batch_size': 64,
         'learning_rate': 0.006, # Res_Cifar: 0.006, Alex_Cifar: 0.0002, Alex_brain: 0.001, Res_brain: 0.0003
@@ -57,7 +57,7 @@ def experiment(conf):
     data_dirs = [
         # {'path': './datasets/Generated_Set1', 'classes': 5, 'name': 'Generated1'},
         # {'path': './datasets/Generated_Set2', 'classes': 75, 'name': 'Generated2'},
-        {'path': './datasets', 'classes': 8, 'name': 'Crop'} 
+        {'path': './datasets', 'classes': 10, 'name': 'Crop'} 
         ]
 
     if conf['architecture'] == 'ViT':

Code/Trainer.py
@@ -36,7 +36,6 @@ class Trainer:
         self._load_data(dataset_name)
 
     def test(self, model, batch_size, criterion):
-
         test_loader = torch.utils.data.DataLoader(self.test_sets[-1], batch_size=batch_size, shuffle=True)
 
         with torch.no_grad():
@@ -226,6 +225,8 @@ class Trainer:
         normalize = transforms.Normalize(
             mean= result['mean'],
             std= result['std'],
+            # mean=[0.485, 0.456, 0.406],
+            # std= [0.229, 0.224, 0.225]
         )
         if architecture == 'ResNet':
             size = 224