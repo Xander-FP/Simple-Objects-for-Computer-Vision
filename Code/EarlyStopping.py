0a4d5b6905aa20b72a51d04b200c7a280a0b945c
Xander Coetzer <xdilo.coetzer@gmail.com>
2023-11-04 20:01:02 +0200

    Add method for early stopping

--- Code/EarlyStopping.py
+++ Code/EarlyStopping.py
@@ -47,6 +47,9 @@ class EarlyStopping:
         if optimizer is not None:
             optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
 
+    def load_checkpoint_for_test(self):
+        pass
+
     def reset(self):
         self.counter = 0
         self.early_stop = False
