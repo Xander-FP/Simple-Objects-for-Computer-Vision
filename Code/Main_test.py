e2677a65255b06669d85e70fb7936bae904503e6
James Butler <u20437863@tuks.co.za>
2023-11-05 21:14:06 +0200

    Update Main_test.py

--- Code/Main_test.py
+++ Code/Main_test.py
@@ -35,17 +35,17 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 OPTIONS = {
         'dataset_name': 'Crop',
         'regression': True,
-        'architecture': 'AlexNet', # 'ViT' or 'AlexNet'
-        'epochs': 13, # Cifar10: 80, Brain: 70
+        'architecture': 'ViT', # 'ViT' or 'AlexNet'
+        'epochs': 10, # Cifar10: 80, Brain: 70
         'batch_size': 128,
-        'learning_rate': 0.00003, # Res_Cifar: 0.006, Alex_Cifar: 0.0002, Alex_brain: 0.001, Res_brain: 0.0003
-        'weight_decay': 0.04, # 0.002 Res_Cifar: 0.002, Alex_Cifar: 0.008, Alex_brain: 0.03, Res_brain: 0.01
-        'momentum': 0.62, # Res_Cifar: 0.15, Alex_Cifar: 0.9, Alex_brain: 0.69, Res_brain: 0.9
+        'learning_rate': 0.008, # Res_Cifar: 0.006, Alex_Cifar: 0.0002, Alex_brain: 0.001, Res_brain: 0.0003
+        'weight_decay': 0.07, # 0.002 Res_Cifar: 0.002, Alex_Cifar: 0.008, Alex_brain: 0.03, Res_brain: 0.01
+        'momentum': 0.45, # Res_Cifar: 0.15, Alex_Cifar: 0.9, Alex_brain: 0.69, Res_brain: 0.9
         'opt': 'sgd',
         'curriculum': False,
         'report_logs': False,
         'should_tune': False,
-        'test_only': True,
+        'test_only': False,
         'scheduler': 'N', # N for no scheduler, B for BabyStep, R for RootP
         'should_restore': False,
         'new_epoch': 0,
